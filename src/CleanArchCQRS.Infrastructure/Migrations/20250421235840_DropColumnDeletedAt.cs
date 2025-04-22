3ea753035a9fd83d53f850d043d6a3a6f5e76970
Oswaldo Castro <oswaldogpc@gmail.com>
2025-04-21 21:00:53 -0300

    Update Migrations
    - Remove column DeletedAt

---
+++ src/CleanArchCQRS.Infrastructure/Migrations/20250421235840_DropColumnDeletedAt.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchCQRS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropColumnDeletedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Mangas");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Mangas",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Mangas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DeletedAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Mangas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DeletedAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Mangas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DeletedAt",
+                value: null);
+        }
+    }
+}
